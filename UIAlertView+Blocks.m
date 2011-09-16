f7d3a948ff3727383450eafcb45c5253fbcab6a3
KAWACHI Takashi <kawachi@tonchidot.com>
2011-09-16 15:36:58 +0900

    Added new init interface
    Conflicts:
    
    	UIAlertView+Blocks.h
    	UIAlertView+Blocks.m

--- UIAlertView+Blocks.m
+++ UIAlertView+Blocks.m
@@ -47,6 +47,56 @@ static CFStringRef RI_BUTTON_ASS_KEY = (CFStringRef)@"com.random-ideas.BUTTONS";
     return self;
 }
 
+- (id)initWithTitle:(NSString *)title 
+			message:(NSString *)message 
+  cancelButtonTitle:(NSString *)cancelButtonTitle 
+	  okButtonTitle:(NSString *)okButtonTitle 
+	   cancelAction:(void (^)(void))cancelAction 
+		   okAction:(void (^)(void))okAction
+{
+	if((self = [self initWithTitle:title message:message delegate:self cancelButtonTitle:cancelButtonTitle otherButtonTitles:okButtonTitle, nil]))
+	{
+		NSMutableArray *buttons = [NSMutableArray array];
+        
+		RIButtonItem *cancelItem = [RIButtonItem itemWithLabel:cancelButtonTitle];
+        cancelItem.action = cancelAction;
+        RIButtonItem *okItem = [RIButtonItem itemWithLabel:okButtonTitle];
+        okItem.action = okAction;
+      
+		[buttons addObject:cancelItem];
+		[buttons addObject:okItem];
+        
+        objc_setAssociatedObject(self, RI_BUTTON_ASS_KEY, buttons, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        
+        [self setDelegate:self];
+	}
+	
+	return self;
+}
+
+- (id)initWithTitle:(NSString *)title 
+			message:(NSString *)message 
+  cancelButtonTitle:(NSString *)cancelButtonTitle 
+	   cancelAction:(void (^)(void))cancelAction 
+{
+	if((self = [self initWithTitle:title message:message delegate:self cancelButtonTitle:cancelButtonTitle otherButtonTitles:nil]))
+	{
+		NSMutableArray *buttons = [NSMutableArray array];
+        
+		RIButtonItem *cancelItem = [RIButtonItem itemWithLabel:cancelButtonTitle ];
+        cancelItem.action = cancelAction;
+		
+		[buttons addObject:cancelItem];
+        
+        objc_setAssociatedObject(self, RI_BUTTON_ASS_KEY, buttons, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        
+        [self setDelegate:self];
+	}
+	
+	return self;
+}
+
+
 - (NSInteger)addButtonItem:(RIButtonItem *)item
 {	
     NSMutableArray *buttonsArray = objc_getAssociatedObject(self, RI_BUTTON_ASS_KEY);	
@@ -57,7 +107,7 @@ static CFStringRef RI_BUTTON_ASS_KEY = (CFStringRef)@"com.random-ideas.BUTTONS";
 	return buttonIndex;
 }
 
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     NSArray *buttonsArray = objc_getAssociatedObject(self, RI_BUTTON_ASS_KEY);
     RIButtonItem *item = [buttonsArray objectAtIndex:buttonIndex];
